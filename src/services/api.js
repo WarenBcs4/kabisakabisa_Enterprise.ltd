93b8df6b111894ec6de43f1844e4c29fa4e0f6db
MikeBcs2 <miketyler2029@gmail.com>
2025-11-09 10:39:10 +0300

    production ready

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://enterprisebackendltd.vercel.app/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://kabisakabisabackendenterpriseltd.vercel.app/api';
 
 // Create axios instance
 const api = axios.create({
